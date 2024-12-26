1414f81ef581abf2d75c5fc973400f7609623b77
Iryna Andriichenko <iryna.andriichenko@gmail.com>
2024-12-26 17:37:32 +0100

    Add converter miles to kilometers

--- src/ua/com/ithillel/homework/lesson02/Converter.java
+++ src/ua/com/ithillel/homework/lesson02/Converter.java
@@ -3,5 +3,8 @@ package ua.com.ithillel.homework.lesson02;
 public class Converter {
     public static void main(String[] args) {
         System.out.println("----Functionality for converting miles to kilometers, kilometers to miles----");
+        double mile = 4.0;
+        double kilometers = mile * 1.61;
+        System.out.println("Converting " + mile + " miles to kilometers: " + kilometers);
     }
 }
