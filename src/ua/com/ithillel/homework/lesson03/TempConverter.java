1112035547a17826c24d9218da7d52a2221c031b
Iryna Andriichenko <iryna.andriichenko@gmail.com>
2024-12-29 13:41:11 +0100

    Add CtoF functionality

--- src/ua/com/ithillel/homework/lesson03/TempConverter.java
+++ src/ua/com/ithillel/homework/lesson03/TempConverter.java
@@ -6,9 +6,18 @@ public class TempConverter {
         double fahrenheit = 30.2;
         double celsius = convertFtoC(fahrenheit);
         System.out.println("----Fahrenheit to Celsius Conversion----");
-        System.out.printf("%.1f °F = %.1f °C",fahrenheit, celsius);
+        System.out.printf("%.1f °F = %.1f °C %n", fahrenheit, celsius);
+        System.out.println("----Celsius to Fahrenheit Conversion----");
+        double celsius2 = 25;
+        double fahrenheit2 = convertCtoF(celsius2);
+        System.out.printf("%.1f °C = %.1f °F", celsius2, fahrenheit2);
     }
-        public static double convertFtoC (double fahrenheit){
-            return (fahrenheit - 32) / 1.8;
-        }
+
+    public static double convertFtoC(double fahrenheit) {
+        return (fahrenheit - 32) / 1.8;
     }
+
+    public static double convertCtoF(double celsius) {
+        return celsius * 1.8 + 32;
+    }
+}
