d3386f00d91dbc260785cb274ebce1c5471376d5
Iryna Andriichenko <iryna.andriichenko@gmail.com>
2025-01-06 17:45:39 +0100

    Add kilometers to miles conversion functionality

--- src/ua/com/ithillel/homework/lesson02/Convertor.java
+++ src/ua/com/ithillel/homework/lesson02/Convertor.java
@@ -5,4 +5,9 @@ public class Convertor {
     public static double milesToKilometers(double miles) {
         return miles * 1.60934;
     }
+
+    public static double kilometersToMiles(double kilometers) {
+        return kilometers / 1.60934;
+    }
+
 }
