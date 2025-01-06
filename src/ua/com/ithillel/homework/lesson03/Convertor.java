07fbe6bb7ecb0baf440c54238b552f07a58dbdaf
Iryna Andriichenko <iryna.andriichenko@gmail.com>
2025-01-06 17:51:18 +0100

    Add Fahrenheit to Celsius conversion functionality

---
+++ src/ua/com/ithillel/homework/lesson03/Convertor.java
@@ -0,0 +1,8 @@
+package ua.com.ithillel.homework.lesson03;
+
+public class Convertor {
+
+    public static double fahrenheitToCelsius(double fahrenheit) {
+        return (fahrenheit - 32) * 5 / 9;
+    }
+}
